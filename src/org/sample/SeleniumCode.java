aa866478c0cd2d1dca77dced88d6a2038616200a
Shahanameet <meet2shahana@gmail.com>
2022-01-08 16:37:03 +0530

    Lines added

--- src/org/sample/SeleniumCode.java
+++ src/org/sample/SeleniumCode.java
@@ -5,6 +5,8 @@ public class SeleniumCode {
 	public void seleniumCourse() {
 		System.out.println("Selenium started");
 	}
-	
+	public void javaCourse() {
+		System.out.println("Java Started");
+	}
 }
 		
