459421fd084a62e4023c5960195b0b2f748ac12f
Shahanameet <meet2shahana@gmail.com>
2022-01-10 23:00:05 +0530

    Del 3 lines

--- src/org/sample/SeleniumCode.java
+++ src/org/sample/SeleniumCode.java
@@ -5,11 +5,5 @@ public class SeleniumCode {
 	public void seleniumCourse() {
 		System.out.println("Selenium started");
 	}
-	public void javaCourse() {
-		System.out.println("Java Started");
 	}
-	public void pythonCourse() {
-		System.out.println("phyton Started");
-	}
-}
 		
